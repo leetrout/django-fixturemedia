e8954e8a8141b6437db131c591a449e663807ff9
Lee Trout <lee@leetrout.com>
2012-09-03 22:18:23 -0400

    Adding app

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
 django-fixturemedia
 ===================
 
-Simple tool to manage media files for fixtures
+Simple tool to manage media files for fixtures.
+
+Put relative paths in your fixtures, put your media files in a media directory
+in you fixtures directory.
+
+Run `collectmedia` and your fixture media will be copied to MEDIA_ROOT.
+
+
+YMMV
+----
+
+Only supports JSON fixtures.
+
+
+Installation
+------------
+
+Download the source code and put the fixturemedia module on your Python Path.
+
+Add `'fixturemedia'` to your installed apps.
+
+
+Usage
+-----
+
+1. Add relative paths to your fixtures for file / image fields.
+
+    [
+        {
+            "pk": 1,
+            "model": "myapp.mymodel",
+            "fields": {
+                "name": "Awesome Sauce",
+                "image": "images/awesome-sauce.jpg"
+            }
+        }
+    ]
+
+2. Add a `media` directory in your fixtures directory. Add the matching
+child directories to match the path in the fixture and place your media file
+in the appropriate location.
+
+    myapp/
+        models.py
+        fixtures/
+            initial_data.json
+            media/
+                images/
+                    awesome-sauce.jpg
+
+3. Run `django-admin.py collectmedia` or ``
+
+    ./manage.py collectmedia
+    This will overwrite any existing files. Proceed? yes
+    Copied images/awesome-sauce.jpg to /tmp/django_media/images/awesome-sauce.jpg
+
+
+TODO
+----
+
+Convert to better command modeled after the excellent staticfiles app.
+Contributions welcome.
---
+++ fixturemedia/management/commands/collectmedia.py
@@ -0,0 +1,58 @@
+import os
+import re
+from shutil import copy
+
+from django.core.management.base import CommandError, NoArgsCommand
+from django.db.models import get_apps
+
+
+file_patt = re.compile(r'"([^"]+?\/[^"]+?\.[^."]+?)"')
+
+
+class Command(NoArgsCommand):
+    
+    def handle(self, *args, **kwargs):
+        from django.conf import settings
+        
+        app_module_paths = []
+        for app in get_apps():
+            if hasattr(app, '__path__'):
+                # It's a 'models/' subpackage
+                for path in app.__path__:
+                    app_module_paths.append(path)
+            else:
+                # It's a models.py module
+                app_module_paths.append(app.__file__)
+        
+        app_fixtures = [os.path.join(os.path.dirname(path), 'fixtures') for path in app_module_paths]
+        
+        json_fixtures = []
+        for fixture_path in app_fixtures:
+            try:
+                root, dirs, files = os.walk(fixture_path).next()
+                for file in files:
+                    if file.rsplit('.', 1)[-1] == 'json':
+                        json_fixtures.append((root, os.path.join(root, file)))
+            except StopIteration:
+                pass
+        
+        confirm = raw_input("This will overwrite any existing files. Proceed? ")
+        if confirm != 'yes':
+            raise CommandError("Media syncing aborted")
+        
+        for root, fixture in json_fixtures:
+            file_paths = file_patt.findall(open(fixture).read())
+            if file_paths:
+                for fp in file_paths:
+                    fixture_media = os.path.join(root, 'media')
+                    fixture_path = os.path.join(fixture_media, fp)
+                    if not os.path.exists(fixture_path):
+                        self.stderr.write("File path (%s) found in fixture but not on disk in (%s) \n" % (fp,fixture_path))
+                        continue
+                    final_dest = os.path.join(settings.MEDIA_ROOT, fp)
+                    dest_dir = os.path.dirname(final_dest)
+                    if not os.path.exists(dest_dir):
+                        os.makedirs(dest_dir)
+                    self.stdout.write('Copied %s to %s\n' % (fp, final_dest))
+                    copy(fixture_path, final_dest)
+                    

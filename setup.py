209472705b09e2bd5b69b0a9596c799394784de9
Lee Trout <lee@leetrout.com>
2013-11-21 00:52:56 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
+
+long_description = """
+Simple tool to manage media files for fixtures.
+
+See the repo home for usage instructions at
+https://github.com/leetrout/django-fixturemedia/
+"""
 
 setup(
     name='django-fixturemedia',
-    version='0.1',
+    version='0.1.2',
     description='Simple tool to manage media files for fixtures.',
-    packages=['fixturemedia', 'fixturemedia.management', 'fixturemedia.management.commands'],
+    long_description=long_description,
+    author='Lee Trout',
+    author_email='leetrout@gmail.com',
+    url='https://github.com/leetrout/django-fixturemedia/',
+    packages=[
+        'fixturemedia',
+        'fixturemedia.management',
+        'fixturemedia.management.commands'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    zip_safe=False,
 )
